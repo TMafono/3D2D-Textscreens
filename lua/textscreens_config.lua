2e007964d5d48b33053c9f50c2985e453612fea5
LarryTheBoss <larrytmoore1@gmail.com>
2019-09-27 18:33:48 -0500

    Changed: Typo and another issue.

--- lua/textscreens_config.lua
+++ lua/textscreens_config.lua
@@ -63,5 +63,5 @@ MaxTextScreenLimit = {
 	user = 1,
 	--superadmin = 1,
 }
--- This is the fall back max textscreens incase the rank could be be found.
+-- This is the fall back max textscreens incase the rank coulddn't be found.
 FailsafeMaxTextscreens = 2
